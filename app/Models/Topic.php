0fda026a972c4db51269df9a33522626877ed899
WesleyLee <liyl.space@gmail.com>
2018-07-19 13:25:18 +0000

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,14 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category() // 文章所属分类
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user() // 文章所属用户
+    {
+        return $this->belongsto(User::class);
+    }
 }
