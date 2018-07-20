d353ce7c4e1361d516ffec386e3eb6a9364e8051
WesleyLee <liyl.space@gmail.com>
2018-07-20 06:42:01 +0000

    User can create reply

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'PagesController@root')->name('root');
 
 // Authentication Routes...
@@ -36,4 +25,4 @@ Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
